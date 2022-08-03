2ca10919701f30ad05137decb4419089ef203b2b
enz-22 <costillaenzoo@gmail.com>
2022-08-03 01:17:31 -0300

    make it better

--- src/main/java/com/porfolio/eac/EacApplication.java
+++ src/main/java/com/porfolio/eac/EacApplication.java
@@ -4,6 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EacApplication { public static void main(String[] args) {
-     SpringApplication.run(EacApplication.class, args);}
+public class EacApplication {
+    // public static void main(String[] args) {SpringApplication.run(EacApplication.class, args);}
 }
